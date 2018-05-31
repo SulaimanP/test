ae382c8783ed79e9da4d8bcf82d00ec5dda9c60f
Sulaiman <sulaiman.panjshiri@medicasoftllc.com>
2018-05-31 16:38:50 -0400

    added chocolate cake

--- hungry.py
+++ hungry.py
@@ -3,6 +3,7 @@ while (i < 2):
     hungry = input("Are you hungry?")
     if hungry == "yes":
         print("Eat kabob")
+        print("Eat chocolate cake")
     elif hungry == "i am fasting":
         print("Same")
     else:
