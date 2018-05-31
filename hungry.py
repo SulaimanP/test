36f759cd4303a43e4cd04e1e2e87f73c6fdc6633
Sulaiman <sulaiman.panjshiri@medicasoftllc.com>
2018-05-31 16:29:04 -0400

    added loop structure

--- hungry.py
+++ hungry.py
@@ -1,7 +1,10 @@
-hungry = input("Are you hungry?")
-if hungry == "yes":
-    print("Eat kabob")
-elif hungry == "i am fasting":
-    print("Same")
-else:
-    print("Go to sleep")
+i = 0
+while (i < 2):
+    hungry = input("Are you hungry?")
+    if hungry == "yes":
+        print("Eat kabob")
+    elif hungry == "i am fasting":
+        print("Same")
+    else:
+        print("Go to sleep")
+    i+=1
