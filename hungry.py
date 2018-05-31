2099ed6ac77b3118a08b965119b647a7540a8c46
Sulaiman <sulaiman.panjshiri@medicasoftllc.com>
2018-05-31 16:13:02 -0400

    added hungry prompt

--- hungry.py
+++ hungry.py
@@ -1,1 +1,7 @@
-print("I'm hungry")
+hungry = input("Are you hungry?")
+if hungry == "yes":
+    print("Eat kabob")
+elif hungry == "i am fasting":
+    print("Same")
+else:
+    print("Go to sleep")
